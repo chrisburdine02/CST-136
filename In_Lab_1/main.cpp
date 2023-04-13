fb863f3e23a73b82141c7dffdda0075f75706a5f
Chris Burdine <chrisburdine02@gmail.com>
2023-04-13 00:20:03 -0700

    Initial Commit

---
+++ In_Lab_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  In_Lab_1
+//
+//  Created by Chris Burdine on 4/13/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
